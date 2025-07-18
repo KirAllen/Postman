028249bd601625f9da376eb20665b697ae93e919
Kir <kir.allyanov@gmail.com>
2025-07-18 17:03:12 +0300

    First commit. MVP stage

---
+++ data/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # path('', views.data_home, name='data_home.html'), # список пунктов: резюме, кандидаты, письма. Пользователи этого не видят
+    path('vacancies/', views.vacancies, name='vacancies'), # Список вакансий
+    path('candidates/', views.candidates, name='candidates'), # Список кандидатов
+    path('dashboard/', views.dashboard, name='dashboard'), # личный кабинет
+    path('templates/', views.templates, name='templates'), # список писем
+    path('candidates/<int:pk>/', views.candidate_detail, name='candidate_detail'), # детали по конкретному кандидату
+    path('register/', views.register, name='register'), # регистрация
+    path('login/', views.user_login, name='login'), # вход
+    path('logout/', views.user_logout, name='logout'), # выход
+    path('vacancy_create/', views.vacancy_create, name='vacancy_create'), # создание вакансии
+    path('vacancy_edit/<int:pk>/', views.vacancy_edit, name='vacancy_edit'), # редактирование конкретной вакансии
+    path('vacancy_detail/<int:pk>/', views.vacancy_detail, name='vacancy_detail'), # детали по конкретной вакансии
+    path('vacancy_delete/<int:pk>/', views.vacancy_delete, name='vacancy_delete'),  # удаление кандидата
+    path('template_create/', views.template_create, name='template_create'), # Создание шаблона письмак
+    path('template_edit/<int:pk>', views.template_edit, name='template_edit'), # редактирование письма
+    path('template_detail/<int:pk>/', views.template_detail, name='template_detail'), # детали по конкретнму письму
+    path('template_delete/<int:pk>/', views.template_delete, name='template_delete'),  # удаление кандидата
+    path('candidate_create/', views.candidate_create, name='candidate_create'), # создание карточки кандидата
+    path('candidate_edit/<int:pk>/', views.candidate_edit, name='candidate_edit'),  # редактирование карточки кандидата
+    path('candidate_detail/<int:pk>/', views.candidate_detail, name='candidate_detail'),  # детали по кандидату
+    path('candidate_delete/<int:pk>/', views.candidate_delete, name='candidate_delete'),  # удаление кандидата
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
